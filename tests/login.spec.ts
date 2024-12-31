e9912feff0ad7a0b7e9487a1d4a64f460584f506
Adam <amichalak60@gmail.com>
2024-12-31 03:41:31 +0200

    first aaa implementation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,24 +2,40 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Login tests", () => {
   test("Log-in with correct credentials is successful", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await page.locator('[data-test="username"]').fill("standard_user");
-    await page.locator('[data-test="password"]').fill("secret_sauce");
+    // Arrange
+    const url = "https://www.saucedemo.com/";
+    const userId = "standard_user";
+    const userPassword = "secret_sauce";
+    const wrongUserId = "wronglogin";
+    const wrongUserPassword = "23rfwfe";
+
+    // Act
+    await page.goto(url);
+    await page.locator('[data-test="username"]').fill(userId);
+    await page.locator('[data-test="password"]').fill(userPassword);
     await page.locator('[data-test="login-button"]').click();
-    await expect(page.locator('[data-test="shopping-cart-link"]')).toBeVisible();
+
+    // Assert
+    await expect(
+      page.locator('[data-test="shopping-cart-link"]')
+    ).toBeVisible();
   });
 
-  test("Log-in with incorrect login returns expected error message", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await page.locator('[data-test="username"]').fill("wronglogin");
+  test("Log-in with incorrect login returns expected error message", async ({
+    page,
+  }) => {
+    await page.goto(url);
+    await page.locator('[data-test="username"]').fill(wrongUserId);
     await page.locator('[data-test="login-button"]').click();
     await expect(page.locator('[data-test="error"]')).toBeVisible();
   });
 
-  test("Log-in with incorrect password returns expected error message", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await page.locator('[data-test="username"]').fill("standard_user");
-    await page.locator('[data-test="password"]').fill("23rfwfe");
+  test("Log-in with incorrect password returns expected error message", async ({
+    page,
+  }) => {
+    await page.goto(url);
+    await page.locator('[data-test="username"]').fill(userId);
+    await page.locator('[data-test="password"]').fill(wrongUserPassword);
     await page.locator('[data-test="login-button"]').click();
     await expect(page.locator('[data-test="error"]')).toBeVisible();
   });
