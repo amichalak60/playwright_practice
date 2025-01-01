ba7c1a2977bf0a49c0a27330e83a0881986ddfaf
Adam <amichalak60@gmail.com>
2025-01-01 14:20:34 +0200

    refactoring DRY implementation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,22 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Login tests', () => {
-  // Arrange
+
+  // Arrange - most commonly used constants
   const userId = 'standard_user';
   const userPassword = 'secret_sauce';
-  const wrongUserId = 'wronglogin';
-  const wrongUserPassword = '23rfwfe';
-  const incorrectLoginExpectedMessage =
-    'Epic sadface: Username and password do not match any user in this service';
-  const noPasswordExpectedMessage = 'Epic sadface: Password is required';
-  const incorrectPasswordExpectedMessage =
-    'Epic sadface: Username and password do not match any user in this service';
 
     test.beforeEach(async ({ page }) => {
       await page.goto('/');
     });
 
   test('Log-in with correct credentials is successful', async ({ page }) => {
+
     // Act
     await page.locator('[data-test="username"]').fill(userId);
     await page.locator('[data-test="password"]').fill(userPassword);
@@ -31,6 +26,12 @@ test.describe('Login tests', () => {
   test('Log-in with incorrect login returns expected error message', async ({
     page,
   }) => {
+
+    // Arrange
+    const wrongUserId = 'wronglogin';
+    const incorrectLoginExpectedMessage =
+    'Epic sadface: Username and password do not match any user in this service';
+
     // Act
     await page.locator('[data-test="username"]').fill(wrongUserId);
     await page.locator('[data-test="password"]').fill(userPassword);
@@ -45,6 +46,10 @@ test.describe('Login tests', () => {
   test('Log-in with no password returns expected error message', async ({
     page,
   }) => {
+
+    //Arrange
+    const noPasswordExpectedMessage = 'Epic sadface: Password is required';
+
     // Act
     await page.locator('[data-test="username"]').fill(userId);
     await page.locator('[data-test="login-button"]').click();
@@ -58,6 +63,12 @@ test.describe('Login tests', () => {
   test('Log-in with incorrect password returns expected error message', async ({
     page,
   }) => {
+
+    // Arrange
+    const wrongUserPassword = '23rfwfe';
+    const incorrectPasswordExpectedMessage =
+    'Epic sadface: Username and password do not match any user in this service';
+
     // Act
     await page.locator('[data-test="username"]').fill(userId);
     await page.locator('[data-test="password"]').fill(wrongUserPassword);
