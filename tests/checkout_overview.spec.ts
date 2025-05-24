a65665680bd6db421813323c79da4cf3588160f3
Adam <amichalak60@gmail.com>
2025-05-24 03:44:08 +0300

    adding dynamic data for checkout overview tests

--- tests/checkout_overview.spec.ts
+++ tests/checkout_overview.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from '../fixtures/fixtures.ts';
 import { loginData } from '../test-data/login.data';
+import { faker } from '@faker-js/faker';
 
 test.describe('Checkout information page tests', () => {
   // arrange - most commonly used variables
@@ -9,9 +10,9 @@ test.describe('Checkout information page tests', () => {
 
   test.beforeEach(async ({ page, inventoryPage, checkoutInformationPage, cartPage }) => {
     //arrange
-    const firstName = 'Adam';
-    const lastName = 'Michalak';
-    const postalCode = '111-234';
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const postalCode = faker.location.zipCode();
     //act
     await page.goto('/inventory.html');
     await inventoryPage.addSixItemsToCart();
